4dc20529b0a5214c759063feb24f265e60027618
ezsinehan <ezsinehan@gmail.com>
2026-03-16 20:05:12 -0700

    feat: auto-kill existing brave instances before launching

--- brave-debug.bat
+++ brave-debug.bat
@@ -1,3 +1,5 @@
 @echo off
-set /p BRAVE_PATH=<brave-path.txt
+taskkill /f /im brave.exe >nul 2>&1
+timeout /t 1 >nul
+set /p BRAVE_PATH=<"%~dp0brave-path.txt"
 start "" "%BRAVE_PATH%" --remote-debugging-port=9222 --new-window
