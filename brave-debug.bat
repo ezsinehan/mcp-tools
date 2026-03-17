7f174a3120f01367a79438719d03487ffbedd390
ezsinehan <ezsinehan@gmail.com>
2026-03-16 20:18:00 -0700

    fix: generic example path, skip comments in bat, support chrome too

--- brave-debug.bat
+++ brave-debug.bat
@@ -1,5 +1,6 @@
 @echo off
 taskkill /f /im brave.exe >nul 2>&1
+taskkill /f /im chrome.exe >nul 2>&1
 timeout /t 1 >nul
-set /p BRAVE_PATH=<"%~dp0brave-path.txt"
-start "" "%BRAVE_PATH%" --remote-debugging-port=9222 --new-window
+for /f "eol=# tokens=*" %%i in ("%~dp0brave-path.txt") do set BROWSER_PATH=%%i
+start "" "%BROWSER_PATH%" --remote-debugging-port=9222 --new-window
